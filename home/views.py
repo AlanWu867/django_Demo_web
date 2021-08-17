abbf96df497903e0e02456a9eeeaf7d6b921f4f5
Alan <50591145+AlanWu867@users.noreply.github.com>
2021-08-17 20:07:46 +0800

    Update views.py

--- home/views.py
+++ home/views.py
@@ -40,7 +40,6 @@ def Inquire(request):
     all_page=math.ceil(val_count/limit)
     page_obj  = Paginator(topics, limit)
     pagerange1=[]
-    # try:
     page_obj  = page_obj.page(1)
     object_list = page_obj.object_list
     pagerange = range(1, 6)
@@ -65,29 +64,7 @@ def Inquire(request):
         for i in pagerange:
             pagerange1.append({'page': i, 'SourceIP': ""})
         pagerange =pagerange1
-    # except PageNotAnInteger:  # 如果页码不是个整数
-    #     page=1
-    #     page_obj  = page_obj.page(1)  # 取第一页的记录
-    #     object_list=page_obj.object_list
-    #     has_other_pages=1 if page_obj.has_other_pages else 0
-    #     has_next = 1 if page_obj.has_next() else 0
-    #     has_previous=1 if page_obj.has_previous() else 0
-    #     previous_page_number = page_obj.previous_page_number() if has_previous == 1 else None
-    #     next_page_number = page_obj.next_page_number() if has_next == 1 else None
-    #     pagerange = range(1, 6)
-    #     if SourceIP != None:
-    #         for i in pagerange:
-    #             pagerange1.append({'page':i,'SourceIP':SourceIP})
-    #         pagerange=pagerange1
-    #         previous_page_number=str(previous_page_number)+"&SourceIP=%s"%SourceIP
-    #         next_page_number = str(next_page_number) + "&SourceIP=%s" % SourceIP
-    #     else:
-    #         for i in pagerange:
-    #             pagerange1.append({'page': i, 'SourceIP': ""})
-    #         pagerange = pagerange1
-    # except EmptyPage:  # 如果页码太大，没有相应的记录
-    #     page_obj  = page_obj.page(page_obj .num_pages)  # 取最后一页的记录
-    # print(pagerange[0])
+
     return render(request,'Inquire.html', {'page_obj ': page_obj, 'pagerange':pagerange,
                                            'has_other_pages':has_other_pages,'has_next':has_next,
                                            'has_previous':has_previous,'page':int(page),'next_page_number':next_page_number,
